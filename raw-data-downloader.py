ab85a79b9b61fe55652ce3a1a19da8bac4fa91de
Riccardo Marconcini <rikkardo91@gmail.com>
2017-11-13 12:30:21 +0100

    Update to cloud 2.0

---
+++ raw-data-downloader.py
@@ -0,0 +1,346 @@
+#######################################################################################################################
+#   Libraries, Modules & API                                                                                          #
+#######################################################################################################################
+import shelve
+import requests
+import time
+import argparse
+import threading
+import datetime
+from influxdb import InfluxDBClient
+
+
+#######################################################################################################################
+#   Global Variables                                                                                                  #
+#######################################################################################################################
+
+USER = ''
+PASSWORD = ''
+ORG = ''
+START = ''
+DEVICE = ''
+INFLUXDB_ADDRESS = ''
+INFLUXDB_PORT = 0
+DB = ''
+REFRESH_TOKEN = ''
+TOKEN = ''
+SPECIAL_CHAR = ''
+REFRESH = 0
+
+
+#######################################################################################################################
+#   Main                                                                                                              #
+#######################################################################################################################
+def main():
+
+    global REFRESH_TOKEN
+
+    # Cycle until it is established the connection to the local instance of InfluxDB.
+    while True:
+        try:
+            print('Connecting to InfluxDB...')
+            influxClient = InfluxDBClient(INFLUXDB_ADDRESS, INFLUXDB_PORT, "root", "root")
+
+            # If the database already exists, the creation is skipped.
+            influxClient.create_database(DB)
+
+            print("Connection to InfluxDB established")
+            break
+        except:
+            print("InfluxDB not ready: retrying...")
+            time.sleep(5)
+
+    # Request a refresh token.
+    print("Acquiring a new refresh token")
+    refresh_token_req = requests.post('https://login.relayr.io/oauth/token?client_id=api-client',
+                                      data={'username': USER, 'password': PASSWORD, 'org': ORG})
+
+    # Parse the JSON received as answer.
+    refresh_token_req_json = refresh_token_req.json()
+
+    print("Refresh token saved")
+    # Save the access_token and the refresh_token.
+    REFRESH_TOKEN = refresh_token_req_json['refreshToken']
+    TOKEN = refresh_token_req_json['accessToken']
+
+    # Create an instance of TokenClass and start it.
+    tokenclass = TokenClass()
+    tokenclass.start()
+
+    # Request the device info.
+    device_req = requests.get('https://cloud.relayr.io/devices/'+DEVICE,
+                              headers={'authorization': 'Bearer '+TOKEN})
+
+    # Parse the JSON received as answer.
+    device_req_json = device_req.json()
+
+    # Save the modelId and and modelVersion.
+    modelId = device_req_json['modelId']
+    modelVersion = str(device_req_json['modelVersion'])
+
+    # Requrst the model info.
+    model_req = requests.get('https://cloud.relayr.io/device-models/'+modelId+'/versions/'+modelVersion,
+                             headers={'authorization': 'Bearer '+TOKEN})
+
+    # Parse the JSON received as answer.
+    model_req_json = model_req.json()
+
+    measurements_names = []
+
+    # Cycle all the measurements.
+    for i in range(len(model_req_json['measurements'])):
+
+        # If there is the special character.
+        if SPECIAL_CHAR is not None:
+
+            # Append to measurements_name only the measurements containing the special char.
+            for char in model_req_json['measurements'][i]['name']:
+                if char == SPECIAL_CHAR:
+                    measurements_names.append(model_req_json['measurements'][i]['name'])
+                pass
+
+        # Otherwise append all the measurements.
+        else:
+            measurements_names.append(model_req_json['measurements'][i]['name'])
+
+    print('Measurements to download:')
+    for i in range(len(measurements_names)):
+        print(measurements_names[i])
+
+    raw_class_list = []
+
+    # Start a thread for every different measurement.
+    for i in range(len(measurements_names)):
+
+        # Create a thread passing the influx client and the name of the measurement to download.
+        thread_tmp = RawClass(influxClient, measurements_names[i])
+
+        # Append the thread to the measurements thread list.
+        raw_class_list.append(thread_tmp)
+
+        # Start the thread.
+        thread_tmp.start()
+
+
+#######################################################################################################################
+#   Functions                                                                                                         #
+#######################################################################################################################
+
+def validate_isodate(isodate):
+    """ The function check if a string is a date in ISO time format
+    :param isodate: timestamp in a format to check if the date is in ISO format
+    :return: Boolean, False if the date is not in ISO format
+    """
+    try:
+        datetime.datetime.strptime(isodate, "%Y-%m-%dT%H:%M:%SZ")
+        print('Date parameter is in a valid ISO format.')
+    except ValueError:
+        pass
+        print('Incorrect data format, should be YYYY-mm-ddTHH:MM:SSZ')
+        return False
+    return True
+
+
+#######################################################################################################################
+#   Classes                                                                                                           #
+#######################################################################################################################
+
+class TokenClass(threading.Thread):
+    """ The class extends the Thread class. When it runs, it requests a new access token with the refresh token and
+        then sleeps until a minute before the expiration of token to run again.
+    """
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global TOKEN
+
+        while True:
+
+            print('Requesting a new access token...')
+            # Request a new token using the refresh token.
+            new_token_request = requests.post('https://login.relayr.io/oauth/refresh?client_id=api-client',
+                                              headers={'Content-Type': 'application/json'},
+                                              json={'refresh_token': REFRESH_TOKEN})
+
+            # Parse the JSON received as answer.
+            new_token_request_json = new_token_request.json()
+
+            # Save the access token.
+            TOKEN = new_token_request_json['accessToken']
+            print('New access token saved')
+
+            # Sleep the thread until a minute before the expiration.
+            time.sleep(new_token_request_json['expiresIn']-60)
+
+
+class RawClass(threading.Thread):
+    """ The class downloads the measurements and saves them in influxdb, then sleeps and start again from the last
+        timestamp.
+    """
+    def __init__(self, influxClient, measurement_name):
+        threading.Thread.__init__(self)
+        self.name = measurement_name
+        self.influxClient = influxClient
+
+    def run(self):
+
+        print('Started download thread for the measurement: '+self.name)
+        global TOKEN
+
+        # The first time the thread runs it should use the start timestamp provided via command line if it is present.
+        # Using a flag to avoid to overwrite with stored timestamp during first start.
+        flag = False
+        last_timestamp = START
+
+        while True:
+
+            # Use as last timestamp the passed parameter if flag is false (only first run)
+            if flag:
+                last_timestamp = self.get_last_timestamp()
+
+            # Request the raw measurements providing the last timestamp, the device id and measurements name.
+            raw_req = requests.get('https://cloud.relayr.io/devices/' + DEVICE +
+                                   '/raw-measurements?measurements=' + self.name +
+                                   '&start=' + last_timestamp,
+                                   headers={'authorization': 'Bearer ' + TOKEN})
+
+            # Parse the received JSON.
+            raw_req_json = raw_req.json()
+
+            data = []
+
+            # If new data are received.
+            if len(raw_req_json) != 0:
+
+                # Cycle all the measurements and append name, timestamp and value in data.
+                for j in range(len(raw_req_json)):
+                    data.append({
+                        'measurement': raw_req_json[j]['name'],
+                        'time': raw_req_json[j]['timestamp'],
+                        'fields': {'value': float(raw_req_json[j]['value'])}
+                    })
+
+                    # Update the last timestamp every time the cycle has measurements.
+                    last_timestamp = raw_req_json[j]['timestamp']
+
+                    print(raw_req_json[j]['timestamp'] + ' - ' + raw_req_json[j]['name'] + ' - ' + str(
+                        raw_req_json[j]['value']))
+
+                try:
+                    # Save data in influxDB.
+                    self.influxClient.write_points(data, database=DB, time_precision="ms")
+                except:
+                    print("Error writing into influxDB")
+                    print(data)
+
+                # Convert the las timestamp in a datetime object.
+                tmp_timestamp = datetime.datetime.strptime(last_timestamp,'%Y-%m-%dT%H:%M:%S.%fZ')
+
+                # Increase the timestamp of 1ms to avoid to download again next time the last measurement.
+                increased_timestamp = str(tmp_timestamp + datetime.timedelta(milliseconds=1))
+
+                # Save the last timestamp with shelves.
+                self.set_last_timestamp(str(increased_timestamp))
+
+                # Set flag true to use last timestamp for next cycle.
+                flag = True
+
+            # Sleep the thread for the refresh period.
+            time.sleep(REFRESH)
+
+    # Set function for the last timestamp using the shelf module
+    def set_last_timestamp(self, timestamp):
+        """ The function saves the timestamp through the shelve module
+        :param timestamp: str
+        :return: None
+        """
+
+        # Open the shelve object.
+        s = shelve.open(str(DB))
+        try:
+            # Save the timestamp as value, the key is the name of the measurement.
+            s[self.name] = timestamp
+        except:
+            print("Impossible access to the settings file. Error with timestamp set.")
+        finally:
+            s.close()
+
+    # Get function for the last timestamp using the shelf module
+    def get_last_timestamp(self):
+        """ The function gets the timestamp through the shelve module
+        :return: str
+        """
+
+        # Open the shelve object.
+        s = shelve.open(str(DB))
+        try:
+            # Get the value using as key the name of the measurements.
+            existing = s[self.name]
+        except:
+            print("Impossible access to the settings file. Error with timestamp get.")
+        finally:
+            s.close()
+        return existing
+
+
+#######################################################################################################################
+#   Parsing Command Line Args                                                                                         #
+#######################################################################################################################
+def parse_args():
+    """ The function parse the args specified in the CLI.
+    :return: parsed args
+    """
+    parser = argparse.ArgumentParser(description="MQTT to InfluxDB")
+    parser.add_argument('--db', type=str, required=True,
+                        help="Name of InfluxDB database where save the data.")
+    parser.add_argument('--device', type=str, required=True,
+                        help="The deviceID of the device to download the measurements.")
+    parser.add_argument('--user', type=str, required=True,
+                        help="The user in relayr cloud.")
+    parser.add_argument('--password', type=str, required=True,
+                        help="The password in relayr cloud.")
+    parser.add_argument('--org', type=str, required=True,
+                        help="The organization in relayr cloud.")
+    parser.add_argument('--start', type=str, required=False, default=None,
+                        help="The starting time from when download data.")
+    parser.add_argument('--influxdb_address', type=str, required=False, default="localhost",
+                        help="The address where is hosted the local instance of InfluxDB.")
+    parser.add_argument('--influxdb_port', type=int, required=False, default=8086,
+                        help="The port where is running the local instance of InfluxDB.")
+    parser.add_argument('--special_char', type=str, required=False, default=None,
+                        help="The char in measurement name used to filter the selection.")
+    parser.add_argument('--refresh', type=int, required=False, default=10,
+                        help="Seconds to wait before downloading again new raw data.")
+    return parser.parse_args()
+
+
+#######################################################################################################################
+#   If main statement                                                                                                 #
+#######################################################################################################################
+if __name__ == '__main__':
+
+    # Assign passed parameters to global variables.
+    args = parse_args()
+    DB = args.db
+    DEVICE = args.device
+    USER = args.user
+    PASSWORD = args.password
+    ORG = args.org
+    START = args.start
+    INFLUXDB_ADDRESS = args.influxdb_address
+    INFLUXDB_PORT = args.influxdb_port
+    SPECIAL_CHAR = args.special_char
+    REFRESH = args.refresh
+
+    # Control if the passed starting date is in the requested format.
+    if START is not None:
+
+        # If the date passed is not valid, setting up the default start: one hour before the script starting.
+        if not validate_isodate(START):
+            START = (datetime.datetime.utcnow() - datetime.timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    else:
+        START = (datetime.datetime.utcnow() - datetime.timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    main()
--- readme.md
+++ readme.md
@@ -1,20 +1,18 @@
-# History Downloader from relayr to InfluxDB
+# Raw Data Downloader
 
 ## Introduction
 <!--A brief description of the purpose and functionality of the project.-->
-The History Downloader is a script written in Python that downloads the readings of a given device registered in the relayr cloud and store them into your local instance of InfluxDB.
+The Raw Data Downloader is a Python 3 script that downloads the raw data of a given device registered in the relayr Cloud 2.0 and afterwards it stores it into an InfluxDB instance.
 
-Relayr created this for giving the possibility to save easily the readings into a time series database and use it for analytics, data management or other purposes.
-The database chosen is InfluxDB because it is open source, very stable and with many integrations (e.g. Grafana).
+You can use this script to have always a 1:1 local copy of the raw data from the cloud.
 
 ## Requirements
-<!--A list of all system requirements and required third-party components.
--->
+<!--A list of all system requirements and required third-party components.-->
 For the correct use of this script, you must have:
 
-- a registered device in the [relayr dashboard](https://developer.relayr.io/);
-- [Python 3.X](https://www.python.org/downloads/) installed on your device with [PIP](https://pip.pypa.io/en/stable/installing/);
-- [InfluxDB](https://docs.influxdata.com/influxdb/v1.1/introduction/installation) installed on your device. 
+- credentials for relayr Cloud 2.0;
+- [Python 3.X](https://www.python.org/downloads/) with [PIP](https://pip.pypa.io/en/stable/installing/);
+- [InfluxDB](https://docs.influxdata.com/influxdb/v1.3/) installed on your device.
 
 ## Installation & Configuration
 <!--Step-by-step instructions, with proper punctuation, on how to install and configure the project.-->
@@ -48,60 +46,67 @@ or update if you have already installed it:
 ### 3) Run the script
 For running the script you have just to browse with your terminal into the folder where you have the script and launch the command, for example:
 
-```$ python3 history_downloader.py --token 1234567890 --device 0987654321 --db DBname --norm 100 --freq 3000```
+```$ sudo python3 /path_to/raw-data-downloader.py --device myDEVICE_ID --db myDB --org myORG --password myPASS --user myUSER```
 	
 Output example:
 
 ```	
-Last Timestamp:  1481533346001
-Start downloading data
-Added  98  new rows in database
-New Last Timestamp:  1481539920001
-Sleeping for the next  3000  seconds...
+Connecting to InfluxDB...
+Connection to InfluxDB established
+Acquiring a new refresh token
+Refresh token saved
+Requesting a new access token...
+New access token saved
+Measurements to download:
+temperature
+humidity
+Started download thread for the measurement: temperature
+Started download thread for the measurement: humidity
+2017-11-13T10:58:51.287Z - humidity - 30
+2017-11-13T10:58:56.556Z - humidity - 30
+2017-11-13T10:58:51.287Z - temperature - 23
+2017-11-13T10:58:56.556Z - temperature - 23
 ``` 
 	
 #### Parameters
 	
 | Parameter |  Type  | Required | Default |                     Example                    |
 |:---------:|:------:|:--------:|:-------:|:----------------------------------------------:|
-|  --token  | string |    yes   |         |       --token P7hTR4rgf5P670q5MzYkNogs8K       |
-|  --device | string |    yes   |         | --device 1112a222-3333-4455-6666-777f7f7f7fff7 |
-|    --db   | string |    yes   |         |                   --db 484cb845-cf5b-4125-9add-e16e8330c342                  |
-|    --freq   | int |    no   |    60     |                   --freq 3000                  |
-|    --host   | string |    no   |     localhost    |                   --host 127.1.4.3                  |
-|    --port   | int |    no   |    8086     |                   --port 4000                  |
-|    --norm   | int |    no   |     1    |                   --norm 100                  |
-|    --timestamp   | int |    no   |     0    |                   --timestamp 1481031795001                  |
-|    --timestampISO   | string |    no   |     0    |                   --timestampISO 2017-01-25T18:33:20.000Z                  |
-
-**token:** the account token you can find in your account page in relayr
-Dashboard;
-
-**device:** the device ID on relayr dashboard;
+| --db  	  | string |    yes   |         | --db my_DBname                                 |
+| --device  | string |    yes   |         | --device 1112a222-3333-4455-6666-777f7f7f7fff7 |
+| --user    | string |    yes   |         | --user my_USER                                 |
+| --password | string |  yes    |         | --password my_USERpassword                     |
+| --org     | string |   yes    |         | --org my_ORG                                   |
+| --start   | string |    no    | local time - 1h | --start 2017-11-13T10:30:15Z           |
+| --influxdb_address | string | no | localhost | --influxdb_address 192.168.0.4            |
+| --influxdb_port | int | no    | 8086    | --influxdb_port 8090                           |
+| --special_char | string | no  | None    | --special_char _                               |
+| --refresh | int	|    no     | 10		  | --refresh 10                                   |
 
 **db:** the name of the local InfluxDB database where you want to save your readings;
 
-**freq:** the frequency of checking the cloud if there are new readings available, in seconds;
+**device:** the deviceID of the device on relayr Cloud 2.0;
 
-**host:** the host of your local InfluxDB instance;
+**user:** the user ID in relayr Cloud 2.0;
 
-**port:** the port of your local InfluxDB instance;
+**password:** the user password in relayr Cloud 2.0;
 
-**norm:** if the readings on the cloud need to be normalized, this is the factor to divide the readings;
+**org:**  the organization in relayr Cloud 2.0;
 
-**timestamp:** the timestamp in UNIX format expressed in milliseconds from when download the readings;
+**start:**  the starting time from when download data from the cloud, in ISO time format;
 
-**timestampISO:** the timestamp in ISO format from when download the readings.
+**influxdb_address:** the host of the InfluxDB instance;
 
-If either *timestamp* or *timestampISO* are not defined, the script downloads the readings from the timestamp of the last downloaded reading +1ms, while if it is the first iteration the default value of current timestamp minus 3 days.
+**influxdb_ port:** the port of the InfluxDB instance;
 
+**special_char:** if it is not `None`, only the measurements name containing this character will be downloaded;
+
+**refresh:** the frequency of checking the cloud if there are new readings available, in seconds.
 
 #### Notes
 
 The script creates a table inside a database for each meaning of your device.
-Note that the script creates also a file called `history_downloader_settings.db`. It is a Berkeley DB created through the shelve module of Python. It should be found in the folder where you run the script and it cointains the timestamp of the last reading received.
-
-The script generates automaticly a new token to ensure that the given one does not expire in the while the script is running. To generate a new token, it is nnecessairy you have at least a project created in your relayr dashboard, but do not worry if you do not have since the script itself will generate a stub app for you.
+Note that the script creates also a file called `YOUR_DB_NAME.db`. It is a Berkeley DB created through the shelve module of Python. It should be found in the folder where you run the script and it cointains the timestamp of the last reading received.
 
 ## License
 <!--The license under which the software will be released. Open-source projects MUST include the MIT License, and closed-source projects MUST include a proprietary license to be discussed with the Documentation team.
